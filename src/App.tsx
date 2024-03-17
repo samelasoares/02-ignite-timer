212fd185bc7b2cce15275e7e84b5126bc7125411
Barbara Soares <barbarasamela90@gmail.com>
2024-03-17 14:58:52 -0300

    feat: Buttons and their stylization were implemented. The styling components tool was also used.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from "./componets/Button";
+
 export function App() {
-
   return (
-    <h1>Hello World</h1>
-  )
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  );
 }
-
