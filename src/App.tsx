87a0604c3fb5d3b68cbf8f1547bacb6dfa7a268d
Barbara Soares <barbarasamela90@gmail.com>
2024-03-18 19:17:29 -0300

    feat: Global styles with styled components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./componets/Button";
 import { defaultTheme } from "./componets/styles/themes/default";
+import { GlobalStyle } from "./componets/styles/themes/global";
 
 export function App() {
   return (
-    <ThemeProvider theme={ defaultTheme}>
-      <Button variant ="primary" />
-      <Button variant ="secondary" />
-      <Button variant ="success" />
-      <Button variant ="danger" />
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
       <Button />
+
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
